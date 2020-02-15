b6bc6da129cfad376c36ac9ac2a436c651ea43fc
AlexRussovits <sasikrus2002@gmail.com>
2020-02-15 18:17:07 +0200

    Доделал работу

--- Query/21_AleksanderRussovits.sql
+++ Query/21_AleksanderRussovits.sql
@@ -65,6 +65,76 @@ GO
 SELECT * FROM DataSmith
 
 --7
+GO
+ALTER VIEW Department1
+AS SELECT * FROM Employee WHERE dept_no =  'd1' OR dept_no = 'd2'
+GO
 
+SELECT * FROM Department1
+SELECT * FROM Employee
+-- 8
+GO
+DROP VIEW Employees2
+GO
+-- В 4 задании также удалится представление, потому что это представление было основано на Employees2
 
+--9
 
+SELECT * FROM Project
+SELECT * FROM Project_View
+
+GO
+INSERT INTO Project_View VALUES ('p2', 'Raketa')
+GO
+
+GO
+INSERT INTO Project_View VALUES ('p7', 'Raketa')
+GO
+
+--10
+
+GO
+CREATE VIEW Employees4
+AS SELECT * FROM Employee WHERE emp_no <= 10000
+WITH CHECK OPTION;
+GO
+
+SELECT * FROM Employees4
+INSERT INTO Employees4 VALUES (22123,'Arvo', 'Kont', 'd3', 2500);
+
+--11
+GO
+CREATE VIEW Employees5
+AS SELECT * FROM Employee WHERE emp_no <= 10000
+GO
+
+INSERT INTO Employees5 VALUES (22123,'Arvo', 'Kont', 'd3', 2500);
+
+-- В 10 идёт проверка чтобы табельный номер был меньше или равно 10000, WITH CHECK OPTION проверяет значение 22123 и выводит ошибку, а в 11 нет.
+
+--12
+GO
+CREATE VIEW DataEmpCheck
+AS SELECT * FROM Works_On WHERE YEAR(enter_data) BETWEEN 2007 AND 2008
+WITH CHECK OPTION;
+GO
+
+UPDATE DataEmpCheck
+SET enter_data = '06.01.2006' WHERE emp_no = 29346
+
+SELECT * FROM DataEmpCheck
+SELECT * FROM Works_On
+DROP VIEW DataEmpCheck
+
+--13
+GO
+CREATE VIEW DataEmpCheck2
+AS SELECT * FROM Works_On WHERE YEAR(enter_data) BETWEEN 2007 AND 2008
+GO
+
+UPDATE DataEmpCheck2
+SET enter_data = '06.01.2006' WHERE emp_no = 29346
+
+SELECT * FROM DataEmpCheck2
+
+-- В 12 произошла ошибка, а в 13 в представление при изменении даты сотрудник с 29346 пропадает, так как по условию должно быть между 2007 и 2008 годом
